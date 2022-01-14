0ccaf520183e0cc6e6f9d75655fbc00a81d59a3e
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-01-14 19:49:07 +0100

    Create pagination.js

---
+++ src/js/pagination.js
@@ -0,0 +1,15 @@
+const left = document.querySelector('.pagination_left');
+const hidding = document.querySelector('.pagination_hidding-box');
+const first = document.querySelector('.pagination_first-page');
+const pageMinusSecond = document.querySelector('.pagination_page-2');
+const pageMinusFirst = document.querySelector('.pagination_page-1');
+const current = document.querySelector('.pagination_current-page');
+const pagePlusFirst = document.querySelector('.pagination_page+1');
+const pagePlusSecond = document.querySelector('.pagination_page+2');
+const last = document.querySelector('.pagination_last-page');
+const right = document.querySelector('.pagination_right');
+
+// const changeCurrentPage = (e) => {
+//   let diff = 
+
+// }
