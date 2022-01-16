145e34ca0b76f4fbec30b5c8c23e36a0680b2b11
Radek <radekie@wp.pl>
2022-01-16 19:22:12 +0100

    klikanie

--- src/js/fetchMovie.js
+++ src/js/fetchMovie.js
@@ -3,21 +3,39 @@ const api = {
   apiKey: '130c7a7ecd86dbb286ae26c3cdcca88c',
 };
 
-const fetchMovieData = async () => {
-const movieCard = document.querySelector('.movie-card');
+// const fetchMovieData = async () => {
+//   const movieCard = document.querySelector('.movie-card');
+//   const movieCards = document.querySelectorAll('.movie-card');
+//   let movieCardsTable = [];
 
-  let movieID = movieCard.getAttribute('data-id');
-  
-  console.log(movieCard)
+//   movieCards.forEach(card => {
+//     //console.log(card.getAttribute('data-id'));
+//     movieCardsTable.push(card.getAttribute('data-id'));
+//   })
+
+//   //console.log(movieCardsTable);
+
+//   let movieID = movieCard.getAttribute('data-id');
+
+//   //console.log(movieCard)
+
+//   const response = await fetch(
+//     `${api.baseUrl}/3/movie/${movieCardsTable[1]}?api_key=${api.apiKey}&language=en-US`,
+//   );
+//   const jsonData = await response.json();
+//     //console.log(jsonData);
+//   return jsonData;
+// };
+
+async function fetchMovieData(movieId) {
 
   const response = await fetch(
-    `${api.baseUrl}/3/movie/${movieID}?api_key=${api.apiKey}&language=en-US`,
+    `${api.baseUrl}/3/movie/${movieId}?api_key=${api.apiKey}&language=en-US`
   );
   const jsonData = await response.json();
-    console.log(jsonData);
   return jsonData;
-};
+}
 
-fetchMovieData();
+//fetchMovieData();
 
 export { fetchMovieData };
--- src/js/movieData.js
+++ src/js/movieData.js
@@ -2,7 +2,7 @@ const watchedBtn = document.querySelector('.watched');
 const queueBtn = document.querySelector('.queue');
   import { fetchMovieData } from './fetchMovie';
 
-const movieCard = document.querySelector('.movie-card');
+const movieCard = document.querySelector('.gallery');
 
 let movieData = {
   photo: '',
@@ -16,50 +16,103 @@ let movieData = {
 };
 
 
-  const renderMovie = async () => {
-    let data = await fetchMovieData();
-    movieData.photo = data.poster_path;
-    movieData.title = data.title;
-    movieData.votesAvarage = data.vote_average;
-    movieData.votes = data.vote_count;
-    movieData.popularity = data.popularity;
-    movieData.orginalTitle = data.original_title;
-    const genresArray = data.genres;
-    let genres = genresArray.map(genresArray => genresArray.name);
-    movieData.genre = genres.toString();
-    movieData.about = data.overview;
-    console.log(movieData);
-
-  
-    document
-      .querySelector('.movie')
-      .insertAdjacentHTML(
-        'afterbegin',
-        `<img class="modal__movie-photo" src="https://image.tmdb.org/t/p/original${movieData.photo}" alt="photo" />`,
-      );
-    document
-      .querySelector('.modal__movie-title')
-      .insertAdjacentHTML('afterbegin', `${movieData.title}`);
-    document.querySelector('.vote').insertAdjacentHTML('afterbegin', `${movieData.votesAvarage}`);
-    document.querySelector('.votes').insertAdjacentHTML('afterbegin', `${movieData.votes}`);
-    document.querySelector('.popularity').insertAdjacentHTML('afterbegin', `${movieData.popularity}`);
-    document
-      .querySelector('.orginal-title')
-      .insertAdjacentHTML('afterbegin', `${movieData.orginalTitle}`);
-    document.querySelector('.genre').insertAdjacentHTML('afterbegin', `${movieData.genre}`);
-    document.querySelector('.modal__about').insertAdjacentHTML('afterbegin', `${movieData.about}`); 
-};
+//   const renderMovie = async () => {
+//     let data = await fetchMovieData();
+//     movieData.photo = data.poster_path;
+//     movieData.title = data.title;
+//     movieData.votesAvarage = data.vote_average;
+//     movieData.votes = data.vote_count;
+//     movieData.popularity = data.popularity;
+//     movieData.orginalTitle = data.original_title;
+//     const genresArray = data.genres;
+//     let genres = genresArray.map(genresArray => genresArray.name);
+//     movieData.genre = genres.toString();
+//     movieData.about = data.overview;
+//     //console.log(movieData);
 
 
-
-
-movieCard.addEventListener('click', renderMovie);
+//     document
+//       .querySelector('.movie')
+//       .insertAdjacentHTML(
+//         'afterbegin',
+//         `<img class="modal__movie-photo" src="https://image.tmdb.org/t/p/original${movieData.photo}" alt="photo" />`,
+//       );
+//     document
+//       .querySelector('.modal__movie-title')
+//       .insertAdjacentHTML('afterbegin', `${movieData.title}`);
+//     document.querySelector('.vote').insertAdjacentHTML('afterbegin', `${movieData.votesAvarage}`);
+//     document.querySelector('.votes').insertAdjacentHTML('afterbegin', `${movieData.votes}`);
+//     document.querySelector('.popularity').insertAdjacentHTML('afterbegin', `${movieData.popularity}`);
+//     document
+//       .querySelector('.orginal-title')
+//       .insertAdjacentHTML('afterbegin', `${movieData.orginalTitle}`);
+//     document.querySelector('.genre').insertAdjacentHTML('afterbegin', `${movieData.genre}`);
+//     document.querySelector('.modal__about').insertAdjacentHTML('afterbegin', `${movieData.about}`);
+// };
 
 
 
 
+//movieCard.addEventListener('click', renderMovie);
 
+document.addEventListener('click', target => {
+  try {
+    let movieId = target.path[2].getAttribute('data-id');
+    if (movieId !== null) {
+      renderMovie(movieId);
+    }
+  } catch (e) {
+  }
+})
 
+function renderMovie(movieId) {
+  clearModal();
+    fetchMovieData(movieId).then(res => {
+      let data = res;
+      movieData.photo = data.poster_path;
+      movieData.title = data.title;
+      movieData.votesAvarage = data.vote_average;
+      movieData.votes = data.vote_count;
+      movieData.popularity = data.popularity;
+      movieData.orginalTitle = data.original_title;
+      const genresArray = data.genres;
+      let genres = genresArray.map(genresArray => genresArray.name);
+      movieData.genre = genres.toString();
+      movieData.about = data.overview;
+      //console.log(movieData);
 
+      document
+        .querySelector('.movie')
+        .insertAdjacentHTML(
+          'afterbegin',
+          `<img class="modal__movie-photo" src="https://image.tmdb.org/t/p/original${movieData.photo}" alt="photo" />`,
+        );
+      document
+        .querySelector('.modal__movie-title')
+        .insertAdjacentHTML('afterbegin', `${movieData.title}`);
+      document.querySelector('.vote').insertAdjacentHTML('afterbegin', `${movieData.votesAvarage}`);
+      document.querySelector('.votes').insertAdjacentHTML('afterbegin', `${movieData.votes}`);
+      document
+        .querySelector('.popularity')
+        .insertAdjacentHTML('afterbegin', `${movieData.popularity}`);
+      document
+        .querySelector('.orginal-title')
+        .insertAdjacentHTML('afterbegin', `${movieData.orginalTitle}`);
+      document.querySelector('.genre').insertAdjacentHTML('afterbegin', `${movieData.genre}`);
+      document
+        .querySelector('.modal__about')
+        .insertAdjacentHTML('afterbegin', `${movieData.about}`);
 
+    });
+}
 
+function clearModal() {
+  document.querySelector('.movie').innerHTML = '';
+  document.querySelector('.modal__movie-title').innerHTML = '';
+  document.querySelector('.vote').innerHTML = '';
+  document.querySelector('.votes').innerHTML = '';
+  document.querySelector('.popularity').innerHTML = '';
+  document.querySelector('.orginal-title').innerHTML = '';
+  document.querySelector('.genre').innerHTML = '';
+  document.querySelector('.modal__about').innerHTML = '';
+}
