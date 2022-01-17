8fd10badcb043807820be37ba93cd078ca62739e
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-01-17 19:21:08 +0100

    updated styles and skipping page numbers

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,15 +1,56 @@
 const left = document.querySelector('.pagination_left');
-const hidding = document.querySelector('.pagination_hidding-box');
+const hiding = document.querySelector('.pagination_hiding-box');
 const first = document.querySelector('.pagination_first-page');
-const pageMinusSecond = document.querySelector('.pagination_page-2');
-const pageMinusFirst = document.querySelector('.pagination_page-1');
+const pageMinusTwo = document.querySelector('.pagination_page-less-two');
+const pageMinusOne = document.querySelector('.pagination_page-less-one');
 const current = document.querySelector('.pagination_current-page');
-const pagePlusFirst = document.querySelector('.pagination_page+1');
-const pagePlusSecond = document.querySelector('.pagination_page+2');
+const pagePlusOne = document.querySelector('.pagination_page-more-one');
+const pagePlusTwo = document.querySelector('.pagination_page-more-two');
 const last = document.querySelector('.pagination_last-page');
 const right = document.querySelector('.pagination_right');
 
-// const changeCurrentPage = (e) => {
-//   let diff = 
+pageMinusTwo.value = -2;
+pageMinusOne.value = -1;
+pagePlusOne.value = 1;
+pagePlusTwo.value = 2;
+right.value = 1;
+left.value = -1;
 
-// }
+const changeCurrentPage = (e) => {
+  e.preventDefault();
+  current.textContent = e.currentTarget.textContent;
+  console.log(current.value);
+
+  pageMinusTwo.textContent = Number(pageMinusTwo.textContent) + Number(e.currentTarget.value);
+  pageMinusOne.textContent = Number(pageMinusOne.textContent) + Number(e.currentTarget.value);
+  pagePlusOne.textContent = Number(pagePlusOne.textContent) + Number(e.currentTarget.value);
+  pagePlusTwo.textContent = Number(pagePlusTwo.textContent) + Number(e.currentTarget.value);
+};
+
+const increment = (e) => {
+  e.preventDefault();
+
+  current.textContent = Number(current.textContent) + Number(right.value);
+  pageMinusTwo.textContent = Number(pageMinusTwo.textContent) + Number(right.value);
+  pageMinusOne.textContent = Number(pageMinusOne.textContent) + Number(right.value);
+  pagePlusOne.textContent = Number(pagePlusOne.textContent) + Number(right.value);
+  pagePlusTwo.textContent = Number(pagePlusTwo.textContent) + Number(right.value);
+};
+
+const decrement=(e)=>{
+  e.preventDefault();
+  
+  current.textContent = Number(current.textContent) - Number(right.value);
+  pageMinusTwo.textContent = Number(pageMinusTwo.textContent) - Number(right.value);
+  pageMinusOne.textContent = Number(pageMinusOne.textContent) - Number(right.value);
+  pagePlusOne.textContent = Number(pagePlusOne.textContent) - Number(right.value);
+  pagePlusTwo.textContent = Number(pagePlusTwo.textContent) - Number(right.value);
+  
+}
+
+pageMinusTwo.addEventListener('click', changeCurrentPage);
+pageMinusOne.addEventListener('click', changeCurrentPage);
+pagePlusOne.addEventListener('click', changeCurrentPage);
+pagePlusTwo.addEventListener('click', changeCurrentPage);
+right.addEventListener('click', increment);
+left.addEventListener('click', decrement);
