1b32204b5cfbc364ba7034b2dc0b2c32fee32937
RoxanaRoxana <roxana0804@gmail.com>
2022-01-17 18:34:54 +0100

    localStorage

--- src/js/fetchMovie.js
+++ src/js/fetchMovie.js
@@ -3,29 +3,6 @@ const api = {
   apiKey: '130c7a7ecd86dbb286ae26c3cdcca88c',
 };
 
-// const fetchMovieData = async () => {
-//   const movieCard = document.querySelector('.movie-card');
-//   const movieCards = document.querySelectorAll('.movie-card');
-//   let movieCardsTable = [];
-
-//   movieCards.forEach(card => {
-//     //console.log(card.getAttribute('data-id'));
-//     movieCardsTable.push(card.getAttribute('data-id'));
-//   })
-
-//   //console.log(movieCardsTable);
-
-//   let movieID = movieCard.getAttribute('data-id');
-
-//   //console.log(movieCard)
-
-//   const response = await fetch(
-//     `${api.baseUrl}/3/movie/${movieCardsTable[1]}?api_key=${api.apiKey}&language=en-US`,
-//   );
-//   const jsonData = await response.json();
-//     //console.log(jsonData);
-//   return jsonData;
-// };
 
 async function fetchMovieData(movieId) {
 
@@ -36,6 +13,5 @@ async function fetchMovieData(movieId) {
   return jsonData;
 }
 
-//fetchMovieData();
 
 export { fetchMovieData };
