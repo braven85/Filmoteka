d887a249d0985d1eb33f57cb7fb4ec3455660ed7
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-01-19 02:56:10 +0100

    Merge branch 'main' into origin/feature/pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,11 +1,14 @@
+// import '../sass/main.css';
 const left = document.querySelector('.pagination_left');
-const hiding = document.querySelector('.pagination_hiding-box');
+const hiding = document.querySelector('.pagination_hider');
+const hidingFirst = document.querySelector('.pagination_hider-first');
+const hidingSecond = document.querySelector('.pagination_hider-second');
 const first = document.querySelector('.pagination_first-page');
-const pageMinusTwo = document.querySelector('.pagination_page-less-two');
-const pageMinusOne = document.querySelector('.pagination_page-less-one');
+const pageMinusTwo = document.querySelector('.pagination_less-two');
+const pageMinusOne = document.querySelector('.pagination_less-one');
 const current = document.querySelector('.pagination_current-page');
-const pagePlusOne = document.querySelector('.pagination_page-more-one');
-const pagePlusTwo = document.querySelector('.pagination_page-more-two');
+const pagePlusOne = document.querySelector('.pagination_more-one');
+const pagePlusTwo = document.querySelector('.pagination_more-two');
 const last = document.querySelector('.pagination_last-page');
 const right = document.querySelector('.pagination_right');
 
@@ -27,9 +30,43 @@ const changeCurrentPage = (e) => {
   pagePlusTwo.textContent = Number(pagePlusTwo.textContent) + Number(e.currentTarget.value);
 };
 
+const conditionalHide = (e) => {
+  if (current.textContent === '1') {
+    left.disabled = true;
+    first.style.visibility = 'hidden';
+    first.disabled=true;
+    hidingFirst.style.visibility = 'hidden';
+    hidingFirst.disabled=true;
+    pageMinusOne.style.visibility = 'hidden';
+    pageMinusOne.disabled=true;
+    pageMinusTwo.style.visibility='hidden';
+    pageMinusTwo.disabled=true;
+  } else if(current.textContent==='2'){
+    left.disabled = false;
+    first.style.visibility = 'hidden';
+    first.disabled=true;
+    hidingFirst.style.visibility = 'hidden';
+    hidingFirst.disabled=true;
+    pageMinusOne.style.visibility = 'hidden';
+    pageMinusOne.disabled=true;
+    pageMinusTwo.style.visibility='visible';
+    pageMinusTwo.disabled=false;
+  } else if(current.textContent==='3'){ }
+};
+
+const skipToFirst = (e) => {
+  e.preventDefault();
+  changeCurrentPage(e);
+  e.currentTarget.style.visibility = 'hidden';
+  e.currentTarget.disabled = true;
+  pageMinusOne.style.visibility = 'hidden';
+  pageMinusOne.disabled = true;
+  pageMinusTwo.style.visibility = 'hidden';
+  pageMinusTwo.disabled = true;
+};
+
 const increment = (e) => {
   e.preventDefault();
-
   current.textContent = Number(current.textContent) + Number(right.value);
   pageMinusTwo.textContent = Number(pageMinusTwo.textContent) + Number(right.value);
   pageMinusOne.textContent = Number(pageMinusOne.textContent) + Number(right.value);
@@ -37,16 +74,15 @@ const increment = (e) => {
   pagePlusTwo.textContent = Number(pagePlusTwo.textContent) + Number(right.value);
 };
 
-const decrement=(e)=>{
+const decrement = (e) => {
   e.preventDefault();
-  
+
   current.textContent = Number(current.textContent) - Number(right.value);
   pageMinusTwo.textContent = Number(pageMinusTwo.textContent) - Number(right.value);
   pageMinusOne.textContent = Number(pageMinusOne.textContent) - Number(right.value);
   pagePlusOne.textContent = Number(pagePlusOne.textContent) - Number(right.value);
   pagePlusTwo.textContent = Number(pagePlusTwo.textContent) - Number(right.value);
-  
-}
+};
 
 pageMinusTwo.addEventListener('click', changeCurrentPage);
 pageMinusOne.addEventListener('click', changeCurrentPage);
@@ -54,3 +90,4 @@ pagePlusOne.addEventListener('click', changeCurrentPage);
 pagePlusTwo.addEventListener('click', changeCurrentPage);
 right.addEventListener('click', increment);
 left.addEventListener('click', decrement);
+first.addEventListener('click', skipToFirst);
